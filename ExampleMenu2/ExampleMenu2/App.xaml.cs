2c1eb4245dcb38162c529123554c70ce166c08b9
ZeroBorn <92119810+ZeroBorn@users.noreply.github.com>
2022-03-21 15:14:30 +0300

    Add files via upload

---
+++ ExampleMenu2/ExampleMenu2/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using ExampleMenu2.Utils;
+using ExampleMenu2.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExampleMenu2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+
+            var modules = ReflectionHelper.CreateAllInstancesOf<IModule>();
+
+            var vm = new MainWindowViewModel(modules);
+            mainWindow.DataContext = vm;
+            mainWindow.Closing += (s, args) => vm.SelectedModule.Deactivate();
+            mainWindow.Show();
+        }
+    }
+}
